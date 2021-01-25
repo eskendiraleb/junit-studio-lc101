2d47daf6771907bad2594ae544412c1ecca6f660
Eskindir <eskindiraleb@yahoo.com>
2021-01-25 16:26:39 -0600

    StudioTesting

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,61 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+
+        assertTrue(true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void bracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void bracketReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void bracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void bracketTest() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][["));
+    }
+
+    @Test
+    public void bracketReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void bracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{[}"));
+    }
+    @Test
+    public void returnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[}"));
+    }
+    @Test
+    public void bracketReturn() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{]"));
+    }
+    @Test
+    public void falseReturn() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("{]{"));
+    }
 
 }
+//", , , "]["
